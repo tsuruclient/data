82cb4e7c77fb41e4eacbc6816d23fd7d2b120246
Origami ONOUE <onoueorigami@gmail.com>
2019-04-13 00:04:52 +0900

    はい

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-	mode: "production",
-	output: {
-		library: '@tsuru/data',
-		libraryTarget: 'umd',
-		filename: 'dist.js',
-	},
-	resolve: {
-		// Add `.ts` and `.tsx` as a resolvable extension.
-		extensions: [".ts", ".tsx", ".js"]
-	},
-	module: {
-		rules: [
-			{ test: /\.tsx?$/, loader: "ts-loader" }
-		]
-	}
+    mode: "production",
+    output: {
+        library: '@tsuru/data',
+        libraryTarget: 'umd',
+        filename: 'dist.js',
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    plugins: []
 }
